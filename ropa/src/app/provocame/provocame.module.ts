1cf661478e1a6056eae71faed220ffe084d6bf03
fernovg <110950631+fernovg@users.noreply.github.com>
2023-06-02 16:22:53 -0600

    Squashed commit of the following:
    commit df326b8fbcccefb5c96af0ac6ec77ce82dc8f60f
    Author: fernovg <110950631+fernovg@users.noreply.github.com>
    Date:   Fri Jun 2 16:17:02 2023 -0600
    
        borrar doc inecesarios
    
    commit b8d48fcde180bd014902fa28a6da1c8feeab306d
    Author: fernovg <110950631+fernovg@users.noreply.github.com>
    Date:   Fri Jun 2 16:09:47 2023 -0600
    
        borrar

--- ropa/src/app/provocame/provocame.module.ts
+++ ropa/src/app/provocame/provocame.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PagesComponent } from './pages/pages.component';
+
 import { InicioComponent } from './pages/inicio/inicio.component';
-import { ComponentsComponent } from './pages/components/components.component';
 
 
 
 @NgModule({
   declarations: [
-    PagesComponent,
     InicioComponent,
-    ComponentsComponent
   ],
   imports: [
     CommonModule
