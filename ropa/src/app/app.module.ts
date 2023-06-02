b8d48fcde180bd014902fa28a6da1c8feeab306d
fernovg <110950631+fernovg@users.noreply.github.com>
2023-06-02 16:09:47 -0600

    borrar

--- ropa/src/app/app.module.ts
+++ ropa/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+//import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
